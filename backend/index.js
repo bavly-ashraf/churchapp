4e13a841889234c3c74d940637432afcfc8bd192
bavly-ashraf <bavly.ashraf2023@gmail.com>
2024-02-10 03:40:23 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
