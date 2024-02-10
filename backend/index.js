78333401b39fb3ada830d4d213257081c24743fa
bavly-ashraf <bavly.ashraf2023@gmail.com>
2024-02-10 03:48:09 +0200

    adding dotenv

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3000;
+require('dotenv').config();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
