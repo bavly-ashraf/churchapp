2a14d0c75ab48da7799fbbad6d6f2727beb028e7
bavly-ashraf <bavly.ashraf2023@gmail.com>
2024-09-15 21:51:37 +0300

    sending scheduled notifications for approved requests only

--- backend/Controllers/reservationController.js
+++ backend/Controllers/reservationController.js
@@ -96,7 +96,7 @@ const scheduledNotification = async (req, res, next) => {
     todaysDate.setHours(0,0,0,0);
     const tomorrowsDate = new Date(todaysDate);
     tomorrowsDate.setDate(todaysDate.getDate() + 1);
-    const todaysReservations = await Reservation.find({startTime:{$gte:todaysDate,$lt:tomorrowsDate},isConfirmed:false},'reserver hall startTime endTime').populate('reserver hall','firebaseToken name -_id');
+    const todaysReservations = await Reservation.find({startTime:{$gte:todaysDate,$lt:tomorrowsDate},status:'Approved',isConfirmed:false},'reserver hall startTime endTime').populate('reserver hall','firebaseToken name -_id');
     todaysReservations.forEach(reservation => {
       sendPushNotification('تأكيد الحجز', `من فضلك دوس هنا عشان تأكد حجزك ل${reservation.hall.name} بكرة في الوقت من ${reservation.startTime.toLocaleString([], {hour: '2-digit',minute: '2-digit'}).replace('PM','م').replace('AM','ص')} ل ${reservation.endTime.toLocaleString([], {hour: '2-digit',minute: '2-digit'}).replace('PM','م').replace('AM','ص')}`,reservation.reserver.firebaseToken,reservation.id);
     });
