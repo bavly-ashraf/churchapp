be6b35400c4be275502063866c5b8c51b2ab9678
bavly-ashraf <bavly.ashraf2023@gmail.com>
2024-10-05 18:03:19 +0300

    removing old job && importing moment

--- backend/Utils/timeZone.js
+++ backend/Utils/timeZone.js
@@ -1,3 +1,5 @@
+ const moment = require('moment');
+ 
  const toLocalTimeZone = (date) => {
         var today = moment(date);
         today.tz('Africa/Cairo').format();
--- backend/index.js
+++ backend/index.js
@@ -26,36 +26,6 @@ admin.initializeApp({
 });
 /////////////////////////////////////////////////////////////////////
 
-/////////////////// Repeatable Job to get users that have reservations today //////////////////
-functions.pubsub.schedule(`0 0 * * *`).timeZone('Africa/Cairo').onRun(async ()=> {
-  const todaysDate = new Date();
-  todaysDate.setHours(0,0,0,0);
-  const tomorrowsDate = new Date(todaysDate);
-  tomorrowsDate.setDate(todaysDate.getDate() + 1);
-  const todaysReservations = await Reservation.find({startTime:{$gte:todaysDate,$lt:tomorrowsDate},isConfirmed:false},'reserver hall startTime endTime').populate('reserver hall','firebaseToken name -_id');
-  todaysReservations.forEach(reservation => {
-    sendPushNotification('تأكيد الحجز', `من فضلك دوس هنا عشان تأكد حجزك ل${reservation.hall.name} بكرة في الوقت من ${reservation.startTime.toLocaleString([], {hour: '2-digit',minute: '2-digit'}).replace('PM','م').replace('AM','ص')} ل ${reservation.endTime.toLocaleString([], {hour: '2-digit',minute: '2-digit'}).replace('PM','م').replace('AM','ص')}`,reservation.reserver.firebaseToken,reservation.id);
-  });
-  // if(usersToken?.length > 0){
-  //   sendPushNotification('تأكيد الحجز', 'من فضلك أكد حجزك',usersToken);
-  // }
-});
-// cron.schedule('0 0 0 * * *', async ()=> {
-//   const todaysDate = new Date();
-//   todaysDate.setHours(0,0,0,0);
-//   const tomorrowsDate = new Date(todaysDate);
-//   tomorrowsDate.setDate(todaysDate.getDate() + 1);
-//   const todaysReservations = await Reservation.find({startTime:{$gte:todaysDate,$lt:tomorrowsDate},isConfirmed:false},'reserver hall startTime endTime').populate('reserver hall','firebaseToken name -_id');
-//   todaysReservations.forEach(reservation => {
-//     sendPushNotification('تأكيد الحجز', `من فضلك دوس هنا عشان تأكد حجزك ل${reservation.hall.name} بكرة في الوقت من ${reservation.startTime.toLocaleString([], {hour: '2-digit',minute: '2-digit'}).replace('PM','م').replace('AM','ص')} ل ${reservation.endTime.toLocaleString([], {hour: '2-digit',minute: '2-digit'}).replace('PM','م').replace('AM','ص')}`,reservation.reserver.firebaseToken,reservation.id);
-//   });
-//   // if(usersToken?.length > 0){
-//   //   sendPushNotification('تأكيد الحجز', 'من فضلك أكد حجزك',usersToken);
-//   // }
-// },{runOnInit:true});
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
